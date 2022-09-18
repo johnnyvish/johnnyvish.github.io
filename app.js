6e622a6de048e3b62f9d2c375018ec5fa920e66c
johnnyvish <58196813+johnnyvish@users.noreply.github.com>
2022-09-17 22:18:12 -0400

    small change

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ function playSound(id) {
 
 pianoKeys.forEach((pianoKey, i) => {
    
-    pianoKey.addEventListener('mousedown', () => {
-        const url = '24-piano-keys/' + pianoKey.id + '.mp3'
+    pianoKey.addEventListener('mouseover', () => {
+        const url = 'audio/' + pianoKey.id + '.mp3'
         pianoKey.style.backgroundColor = 'red'
         playSound(url)
     })
 
+
     // if pianoKey has an id that contains "#", change the key color to black
-    pianoKey.addEventListener('mouseup', () => pianoKey.style.backgroundColor = pianoKey.id.includes('sharp') ? 'black' : 'white')
+    pianoKey.addEventListener('mouseout', () => pianoKey.style.backgroundColor = pianoKey.id.includes('sharp') ? 'black' : 'white')
 
     
 
